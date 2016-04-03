4a4646267cff7035515b0a71b609b2b64f2ddb66
Anna McDougald <texas.tiel@gmail.com>
2016-04-03 18:10:45 -0500

    Fixed a few things in Purchase

--- JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/dao/impl/PurchaseDaoImpl.java
+++ JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/dao/impl/PurchaseDaoImpl.java
@@ -1,7 +1,11 @@
 package cs4347.jdbcProject.ecomm.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import cs4347.jdbcProject.ecomm.dao.PurchaseDAO;
@@ -20,12 +24,11 @@ public class PurchaseDaoImpl implements PurchaseDAO
 		try{
 			if(purchase.getId() != null){ throw new DAOException("Cannot insert a purchase with a non-null id"); }
 			
-			statement = connection.prepareStatement("INSERT INTO PURCHASE (id, prodName, prodDescription, prodCategory, prodUPC) VALUES(?,?,?,?,?);", Statement.RETURN_GENERATED_KEYS);
-			statement.setString(1, purchase.getId());
-			statement.setString(2, purchase.getProductID());
+			statement = connection.prepareStatement("INSERT INTO PURCHASE (productID, customerID, purchaseDate, purchaseAmount) VALUES(?,?,?,?);", Statement.RETURN_GENERATED_KEYS);
+			statement.setLong(1, purchase.getProductID());
+			statement.setLong(2, purchase.getCustomerID());
 			statement.setDate(3, purchase.getPurchaseDate());
-			statement.setString(4, purchase.getCustomerID() + "");
-			statement.setString(5, purchase.getPurchaseAmount());
+			statement.setDouble(4, purchase.getPurchaseAmount());
 			statement.executeUpdate();
 			
 			ResultSet keys = statement.getGeneratedKeys();
@@ -43,24 +46,22 @@ public class PurchaseDaoImpl implements PurchaseDAO
 
 	@Override
 	public Purchase retrieve(Connection connection, Long id) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		//return null;
 		PreparedStatement statement = null;
 		try{
 			if(id == null){ throw new DAOException("Cannot retrieve a purchase with a null id"); }
 			
-			statement = connection.prepareStatement("SELECT id, productID, purchaseDate, customerID, purchaseAmount FROM customer where id = ?;");
+			statement = connection.prepareStatement("SELECT id, productID, customerID, purchaseDate, purchaseAmount FROM purchase where id = ?;");
 			statement.setLong(1, id);
 			
 			ResultSet result = statement.executeQuery();
 			if(!result.next()) { return null; }
 			
 			Purchase purchase = new Purchase();
-			purchase.setId(result.getLong("id"));
-			purchase.setProductID(result.getString("productID"));
-			purchase.setPurchaseDate(result.getString("purchaseDate"));
-			purchase.setCustomerID(result.getString("customerID"));
-			purchase.setPurchaseAmount(result.getString("purchaseAmount"));
+			purchase.setId(id);
+			purchase.setProductID(result.getLong("productID"));
+			purchase.setCustomerID(result.getLong("customerID"));
+			purchase.setPurchaseDate(result.getDate("purchaseDate"));
+			purchase.setPurchaseAmount(result.getDouble("purchaseAmount"));
 			
 			return purchase;
 		}catch(SQLException e){
@@ -72,17 +73,15 @@ public class PurchaseDaoImpl implements PurchaseDAO
 
 	@Override
 	public int update(Connection connection, Purchase purchase) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		//return 0;
 		PreparedStatement statement = null;
 		try{
 			if(purchase.getId() == null){ throw new DAOException("Cannot update a purchase with a null id"); }
 			
-			statement = connection.prepareStatement("UPDATE purchase SET id = ?, productID = ?, purchaseDate = ?, customerID = ?, purchaseAmount = ? WHERE id = ?;");
-			statement.setString(1, purchase.getID());
-			statement.setString(2, purchase.getProductID());
-			statement.setDate(3, purchase.getPurchaseDate());
-			statement.setString(4, purchase.getCustomerID());
+			statement = connection.prepareStatement("UPDATE purchase SET id = ?, productID = ?, customerID = ?, purchaseDate = ?, purchaseAmount = ? WHERE id = ?;");
+			statement.setLong(1, purchase.getId());
+			statement.setLong(2, purchase.getProductID());
+			statement.setLong(3, purchase.getCustomerID());
+			statement.setDate(4, purchase.getPurchaseDate());
 			statement.setDouble(5, purchase.getPurchaseAmount());
 
 			return statement.executeUpdate();
@@ -96,8 +95,6 @@ public class PurchaseDaoImpl implements PurchaseDAO
 
 	@Override
 	public int delete(Connection connection, Long id) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		//return 0;
 		PreparedStatement statement = null;
 		try{
 			if(id == null){ throw new DAOException("Cannot delete a purchase with a null id"); }
@@ -117,25 +114,28 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	@Override
 	public List<Purchase> retrieveForCustomerID(Connection connection, Long customerID)
 			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		//return null;
 		PreparedStatement statement = null;
+		List<Purchase> result = new ArrayList<Purchase>();
 		try{
 			if(customerID == null){ throw new DAOException("Cannot retrieve a purchase with a null id"); }
 			
 			statement = connection.prepareStatement("SELECT id, productID, purchaseDate, purchaseAmount FROM purchase where customerID = ?;");
 			statement.setLong(1, customerID);
 			
-			ResultSet result = statement.executeQuery();
-			if(!result.next()) { return null; }
+			ResultSet set = statement.executeQuery();
+			if(!set.next()) { return null; }
 			
-			Purchase purchase = new Purchase();
-			purchase.setID(result.getString("id"));
-			purchase.setProductID(result.getString("productID"));
-			purchase.setPurchaseDate(result.getString("purchaseDate"));
-			purchase.setPurchaseAmount(result.getString("purchaseAmount"));
-			return purchase;
-			
+			while(set.next()){
+				
+				Purchase purchase = new Purchase();
+				purchase.setId(set.getLong("id"));
+				purchase.setProductID(set.getLong("productID"));
+				purchase.setPurchaseDate(set.getDate("purchaseDate"));
+				purchase.setPurchaseAmount(set.getDouble("purchaseAmount"));
+				result.add(purchase);
+				
+			}
+			return result;
 		}catch(SQLException e){
 			throw new DAOException(e.getMessage());
 		}finally{
@@ -146,25 +146,28 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	@Override
 	public List<Purchase> retrieveForProductID(Connection connection, Long productID)
 			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		//return null;
 		PreparedStatement statement = null;
+		List<Purchase> result = new ArrayList<Purchase>();
 		try{
 			if(productID == null){ throw new DAOException("Cannot retrieve a purchase with a null id"); }
 			
-			statement = connection.prepareStatement("SELECT id, productID, purchaseDate, purchaseAmount FROM purchase where productID = ?;");
+			statement = connection.prepareStatement("SELECT id, customerID, purchaseDate, purchaseAmount FROM purchase where productID = ?;");
 			statement.setLong(1, productID);
 			
-			ResultSet result = statement.executeQuery();
-			if(!result.next()) { return null; }
+			ResultSet set = statement.executeQuery();
+			if(!set.next()) { return null; }
 			
-			Purchase purchase = new Purchase();
-			purchase.setID(result.getString("id"));
-			purchase.setProductID(result.getString("productID"));
-			purchase.setPurchaseDate(result.getString("purchaseDate"));
-			purchase.setPurchaseAmount(result.getString("purchaseAmount"));
-			return purchase;
-			
+			while(set.next()){
+				
+				Purchase purchase = new Purchase();
+				purchase.setId(set.getLong("id"));
+				purchase.setCustomerID(set.getLong("customerID"));
+				purchase.setPurchaseDate(set.getDate("purchaseDate"));
+				purchase.setPurchaseAmount(set.getDouble("purchaseAmount"));
+				result.add(purchase);
+				
+			}
+			return result;
 		}catch(SQLException e){
 			throw new DAOException(e.getMessage());
 		}finally{
@@ -175,25 +178,29 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	@Override
 	public PurchaseSummary retrievePurchaseSummary(Connection connection, Long customerID)
 			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		//return null;
 		PreparedStatement statement = null;
+		PurchaseSummary summary = new PurchaseSummary();
 		try{
-			if(customerID == null){ throw new DAOException("Cannot retrieve a customer with a null id"); }
+			if(customerID == null){ throw new DAOException("Cannot retrieve a purchase with a null id"); }
 			
-			statement = connection.prepareStatement("SELECT id, productID, purchaseDate, purchaseAmount FROM purchase where productID = ?;");
-			statement.setLong(1, productID);
+			statement = connection.prepareStatement("SELECT purchaseAmount FROM purchase where customerID = ?;");
+			statement.setLong(1, customerID);
 			
-			ResultSet result = statement.executeQuery();
-			if(!result.next()) { return null; }
+			ResultSet set = statement.executeQuery();
+			if(!set.next()) { return null; }
 			
-			Purchase purchase = new Purchase();
-			purchase.setID(result.getString("id"));
-			purchase.setProductID(result.getString("productID"));
-			purchase.setPurchaseDate(result.getString("purchaseDate"));
-			purchase.setPurchaseAmount(result.getString("purchaseAmount"));
-			return purchase;
-			
+			long total = 0;
+			long current = 0;
+			int numPurchases = 0;
+			while(set.next()){
+				current = set.getLong("purchaseAmount");
+				if(current < summary.minPurchase){ summary.minPurchase = current; }
+				if(current > summary.maxPurchase){ summary.maxPurchase = current; }
+				numPurchases++;
+				total += current;
+			}
+			summary.avgPurchase = (total/numPurchases);
+			return summary;
 		}catch(SQLException e){
 			throw new DAOException(e.getMessage());
 		}finally{
